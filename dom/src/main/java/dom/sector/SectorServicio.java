5c0dc0668bba6e4186028aaa467bee89c7c9718d
Munoz Daniel <munozda87@hotmail.com>
2014-05-22 23:10:38 -0300

    Metodo Listar Todos los Sectores

--- dom/src/main/java/dom/sector/SectorServicio.java
+++ dom/src/main/java/dom/sector/SectorServicio.java
@@ -22,29 +22,43 @@ public class SectorServicio {
 	public String iconName() {
 		return "ownedBy";
 	}
+
 	// //////////////////////////////////////
 	// Insertar un Sector.
 	// //////////////////////////////////////
 	@MemberOrder(sequence = "10")
 	public Sector agregar(
-			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Nombre") String nombreSector)
-	{
-		return nuevoSector(nombreSector,this.currentUserName());
+			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Nombre") String nombreSector) {
+		return nuevoSector(nombreSector, this.currentUserName());
 	}
+
 	@Programmatic
-	public Sector nuevoSector(final String nombreSector,final String creadoPor)
-	{
-		final Sector unSector = this.container.newTransientInstance(Sector.class);
+	public Sector nuevoSector(final String nombreSector, final String creadoPor) {
+		final Sector unSector = this.container
+				.newTransientInstance(Sector.class);
 		unSector.setNombreSector(nombreSector);
 		unSector.setHabilitado(true);
 		unSector.setCreadoPor(creadoPor);
 		this.container.persistIfNotAlready(unSector);
 		this.container.flush();
 		return unSector;
-		
-		
+
 	}
-	
+
+	// //////////////////////////////////////
+	// ListarTodos
+	// //////////////////////////////////////
+	@MemberOrder(sequence = "20")
+	public List<Sector> listarTodos() {
+		final List<Sector> listarSectores = this.container
+				.allMatches(new QueryDefault<Sector>(Sector.class,
+						"todosLosSectores", "creadoPor", this.currentUserName()));
+		if (listarSectores.isEmpty())
+			this.container
+					.warnUser("No se encontraron sectores cargados en el sistema.");
+		return listarSectores;
+	}
+
 	// //////////////////////////////////////
 	// CurrentUserName
 	// //////////////////////////////////////
