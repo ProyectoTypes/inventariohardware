2074fe26a0e065357f45f7f2cb45a83d610304d3
Munoz Daniel <munozda87@hotmail.com>
2014-06-18 22:05:39 -0300

    Tecnico: Cambiado la expresion regular de email

--- dom/src/main/java/dom/tecnico/TecnicoRepositorio.java
+++ dom/src/main/java/dom/tecnico/TecnicoRepositorio.java
@@ -43,8 +43,9 @@ public class TecnicoRepositorio {
 	public Tecnico addTecnico(final @Optional Sector sector,
 			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Apellido") String apellido,
 			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Nombre") String nombre,
-			final @Optional @RegEx(validation = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@ [A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$") @Named("E-mail") String email
+			final @Optional @RegEx(validation = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$") @Named("E-mail") String email
 			) {
+		
 		return nuevoTecnico(apellido, nombre, email,sector, this.currentUserName());
 	}
 	
@@ -76,7 +77,7 @@ public class TecnicoRepositorio {
 	@Named("Sector")
 	@DescribedAs("Buscar el Sector en mayuscula")
 	public List<Sector> autoComplete0AddTecnico(final @MinLength(2) String search) {
-		return sectorRepositorio.autoCompleteSector(search);
+		return sectorRepositorio.autoComplete(search);
 	}
 	
 	// //////////////////////////////////////
