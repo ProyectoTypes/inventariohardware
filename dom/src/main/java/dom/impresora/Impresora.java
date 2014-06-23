e2cdf9d0e7efba975fd6434fff8abcfd7f3a4602
Oscar Eduardo Sepulveda <oscarsepulveda85@gmail.com>
2014-06-23 16:03:12 -0300

    Metodo creado por en Impresora

--- dom/src/main/java/dom/impresora/Impresora.java
+++ dom/src/main/java/dom/impresora/Impresora.java
@@ -3,20 +3,22 @@ package dom.impresora;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.apache.isis.applib.annotation.DescribedAs;
+import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.RegEx;
+import org.apache.isis.applib.annotation.Where;
 
 public class Impresora {
 
 	// //////////////////////////////////////
 	// Identificacion en la UI. Aparece como item del menu
 	// //////////////////////////////////////
-	
-	public String title(){
+
+	public String title() {
 		return this.getNombreImpresora();
 	}
-	
-	public String iconName(){
+
+	public String iconName() {
 		return "Impresora";
 	}
 
@@ -25,7 +27,8 @@ public class Impresora {
 	// //////////////////////////////////////
 
 	private String nombreImpresora;
-	@javax.jdo.annotations.Column(allowsNull= "false")
+
+	@javax.jdo.annotations.Column(allowsNull = "false")
 	@RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*")
 	@DescribedAs("Nombre de la Impresora:")
 	@MemberOrder(sequence = "10")
@@ -35,6 +38,22 @@ public class Impresora {
 
 	public void setNombreImprsora(String nombreImpresora) {
 		this.nombreImpresora = nombreImpresora;
+	}
+
+	// //////////////////////////////////////
+	// creadoPor
+	// //////////////////////////////////////
+
+	private String creadoPor;
+
+	@Hidden(where = Where.ALL_TABLES)
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
 	}
 
 }
