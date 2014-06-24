59025ef776d9352672cde9607e88e0d7fc6bb67b
Oscar Eduardo Sepulveda <oscarsepulveda85@gmail.com>
2014-06-24 16:17:28 -0300

    Agregando Anotaciones JDO

--- dom/src/main/java/dom/impresora/Impresora.java
+++ dom/src/main/java/dom/impresora/Impresora.java
@@ -1,12 +1,50 @@
 package dom.impresora;
 
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.apache.isis.applib.annotation.Audited;
+import org.apache.isis.applib.annotation.AutoComplete;
+import org.apache.isis.applib.annotation.Bookmarkable;
 import org.apache.isis.applib.annotation.DescribedAs;
 import org.apache.isis.applib.annotation.Disabled;
 import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ObjectType;
 import org.apache.isis.applib.annotation.RegEx;
 import org.apache.isis.applib.annotation.Where;
 
+import dom.computadora.ComputadoraRepositorio;
+
+@javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "id")
+@javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
+@javax.jdo.annotations.Uniques({ @javax.jdo.annotations.Unique(name = "Impresora_nombreImpresora_must_be_unique", members = {
+		"creadoPor", "nombreImpresora" }) })
+@javax.jdo.annotations.Queries({
+		@javax.jdo.annotations.Query(name = "autoCompletePorNombreImpresora", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.impresora.Impresora "
+				+ "WHERE creadoPor == :creadoPor && nombreImpresora.indexOf(:nombreImpresora) >= 0"),
+		@javax.jdo.annotations.Query(name = "todasLasImpresoras", language = "JDOQL", value = "SELECT FROM dom.impresora.Impresora "
+				+ " WHERE creadoPor == :creadoPor && habilitado == true"),
+		@javax.jdo.annotations.Query(name = "eliminarImpresoraFalse", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.impresora.Impresora "
+				+ "WHERE creadoPor == :creadoPor "
+				+ "   && habilitado == true"),
+		@javax.jdo.annotations.Query(name = "eliminarImpresoraTrue", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.impresora.Impresora "
+				+ "WHERE creadoPor == :creadoPor "
+				+ "   && habilitado == true"),
+		@javax.jdo.annotations.Query(name = "buscarPorNombreImpresora", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.impresora.Impresora "
+				+ "WHERE creadoPor == :creadoPor "
+				+ "   && nombreSector.indexOf(:nombreImpresora) >= 0") })
+
+
+@ObjectType("COMPUTADORA")
+@Audited
+@AutoComplete(repository = ComputadoraRepositorio.class, action = "autoComplete")
+@Bookmarkable
 public class Impresora {
 
 	// //////////////////////////////////////
