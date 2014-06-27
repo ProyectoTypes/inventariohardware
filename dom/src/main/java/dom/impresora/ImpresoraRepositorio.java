8374932615e6a80a0854a673e3678b163ab34c5e
Oscar Eduardo Sepulveda <oscarsepulveda85@gmail.com>
2014-06-26 21:42:43 -0300

    Agregando icono a ImpresoraRepositorio

--- dom/src/main/java/dom/impresora/ImpresoraRepositorio.java
+++ dom/src/main/java/dom/impresora/ImpresoraRepositorio.java
@@ -2,4 +2,15 @@ package dom.impresora;
 
 public class ImpresoraRepositorio {
 
+	// //////////////////////////////////////
+		// Icono
+		// //////////////////////////////////////
+
+		public String title() {
+			return "IMPRESORA";
+		}
+
+		public String iconName() {
+			return "IMPRESORA";
+		}
 }
