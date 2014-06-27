7eb973315b245c5214ffb4aaeac59c939fcacf35
Oscar Eduardo Sepulveda <oscarsepulveda85@gmail.com>
2014-06-26 21:51:45 -0300

    Agregando CurrentUserName e Injected Services

--- dom/src/main/java/dom/impresora/ImpresoraRepositorio.java
+++ dom/src/main/java/dom/impresora/ImpresoraRepositorio.java
@@ -1,16 +1,41 @@
 package dom.impresora;
 
+import org.apache.isis.applib.DomainObjectContainer;
+
+import dom.usuario.UsuarioRepositorio;
+
 public class ImpresoraRepositorio {
 
 	// //////////////////////////////////////
-		// Icono
-		// //////////////////////////////////////
+	// Icono
+	// //////////////////////////////////////
 
-		public String title() {
-			return "IMPRESORA";
-		}
+	public String title() {
+		return "IMPRESORA";
+	}
 
-		public String iconName() {
-			return "IMPRESORA";
-		}
+	public String iconName() {
+		return "IMPRESORA";
+	}
+
+	// //////////////////////////////////////
+	// CurrentUserName
+	// //////////////////////////////////////
+
+	private String currentUserName() {
+		return container.getUser().getName();
+	}
+
+	// //////////////////////////////////////
+	// Injected Services
+	// //////////////////////////////////////
+
+	@javax.inject.Inject
+	private DomainObjectContainer container;
+
+	@javax.inject.Inject
+	private ImpresoraRepositorio impresoraRepositorio;
+
+	@javax.inject.Inject
+	private UsuarioRepositorio usuarioRepositorio;
 }
