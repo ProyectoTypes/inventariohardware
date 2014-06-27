0f8f54e27110974461058a77f041954f56d7dc0a
Exequiel Wiedermann <EWFWiedermann@gmail.com>
2014-06-26 21:20:30 -0300

    Creando la entidad MovimientoRepositoriO

---
+++ dom/src/main/java/dom/movimiento/MovimientoRepositorio.java
@@ -0,0 +1,20 @@
+package dom.movimiento;
+
+import java.util.List;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.DescribedAs;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.MinLength;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Optional;
+import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.query.QueryDefault;
+
+import dom.computadora.Computadora;
+import dom.tecnico.Tecnico;
+
+@Named("MOVIMIENTO")
+public class MovimientoRepositorio {
+
+}
