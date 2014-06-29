83ec0cbc9497c5c1646bf678616bdcc194028196
munozdaniel <cipoleto@gmail.com>
2014-06-29 16:20:51 -0300

    Movimiento: Se agrego operación para comparar Movimientos. - Movimiento implementa la interface Comparable.

--- dom/src/main/java/dom/movimiento/Movimiento.java
+++ dom/src/main/java/dom/movimiento/Movimiento.java
@@ -1,56 +1,64 @@
 package dom.movimiento;
 
+import java.util.List;
+
+import javax.jdo.annotations.Column;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.VersionStrategy;
 
+import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.Audited;
 import org.apache.isis.applib.annotation.AutoComplete;
 import org.apache.isis.applib.annotation.Bookmarkable;
-import org.apache.isis.applib.annotation.DescribedAs;
 import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.MinLength;
 import org.apache.isis.applib.annotation.ObjectType;
+import org.apache.isis.applib.annotation.Optional;
+import org.apache.isis.applib.annotation.Where;
+import org.apache.isis.applib.util.ObjectContracts;
 
 import dom.computadora.Computadora;
+import dom.computadora.ComputadoraRepositorio;
 import dom.tecnico.Tecnico;
+import dom.tecnico.TecnicoRepositorio;
 
 @javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
 @javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "id")
 @javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
+// @javax.jdo.annotations.Uniques({ @javax.jdo.annotations.Unique(name =
+// "Sector_nombreSector_must_be_unique", members = {
+// "creadoPor", "nombreSector" }) })
 @javax.jdo.annotations.Queries({
-		@javax.jdo.annotations.Query(name = "autoCompletePorMovimiento", language = "JDOQL", value = "SELECT "
-				+ "FROM dom.movimiento.Movimiento "
-				+ "WHERE ingresadoPor == :ingresadoPor && " + "ip.indexOf(:ip) >= 0"),
-		@javax.jdo.annotations.Query(name = "eliminarMovimientoFalse", language = "JDOQL", value = "SELECT "
-				+ "FROM dom.movimiento.Movimiento "
-				+ "WHERE ingresadoPor == :ingresadoPor "
-				+ "   && habilitado == false"),
-		@javax.jdo.annotations.Query(name = "eliminarMovimientoTrue", language = "JDOQL", value = "SELECT "
-				+ "FROM dom.movimiento.Movimiento "
-				+ "WHERE ingresadoPor == :ingresadoPor " + "   && habilitado == true"),
-		@javax.jdo.annotations.Query(name = "buscarPorIp", language = "JDOQL", value = "SELECT "
-				+ "FROM dom.movimiento.Movimiento "
-				+ "WHERE ingresadoPor == :ingresadoPor "
-				+ "   && ip.indexOf(:ip) >= 0"), })
+	@javax.jdo.annotations.Query(name = "autoCompletePorMovimiento", language = "JDOQL", value = "SELECT "
+			+ "FROM dom.movimiento.Movimiento "
+			+ "WHERE creadoPor == :creadoPor && " + "tecnico.getNombre().indexOf(:buscarTecnico) >= 0"),
+	@javax.jdo.annotations.Query(name = "listar", language = "JDOQL", value = "SELECT "
+			+ "FROM dom.movimiento.Movimiento "
+			+ "WHERE ingresadoPor == :ingresadoPor "
+			+ "   && habilitado == true"),
+	@javax.jdo.annotations.Query(name = "buscarPorIp", language = "JDOQL", value = "SELECT "
+			+ "FROM dom.movimiento.Movimiento "
+			+ "WHERE ingresadoPor == :ingresadoPor "
+			+ "   && computadora.getIp().indexOf(:ip) >= 0"), })
 @ObjectType("MOVIMIENTO")
 @Audited
-@AutoComplete(repository = MovimientoRepositorio.class, action = "autoCompletePorMovimiento")
+@AutoComplete(repository = MovimientoRepositorio.class, action = "autoComplete")
 @Bookmarkable
+public class Movimiento  implements Comparable<Movimiento>{
 
-public class Movimiento {
-	
 	// //////////////////////////////////////
-	// Identificacion en la UI
+	// Identificacion en la UI. Aparece como item del menu
 	// //////////////////////////////////////
 
 	public String title() {
-		return null;
+		return this.creadoPor;
 	}
 
 	public String iconName() {
 		return "Movimiento";
 	}
-		
+
 	// //////////////////////////////////////
 	// Habilitado (propiedad)
 	// //////////////////////////////////////
@@ -66,55 +74,80 @@ public class Movimiento {
 	public void setHabilitado(final boolean habilitado) {
 		this.habilitado = habilitado;
 	}
-		
+
 	// //////////////////////////////////////
-	// Computadora (propiedad)
+	// creadoPor
 	// //////////////////////////////////////
 
-	public Computadora computadora;
+	private String creadoPor;
 
+	@Hidden(where = Where.ALL_TABLES)
 	@javax.jdo.annotations.Column(allowsNull = "false")
-	@DescribedAs("Computadora previamente cargada:")
-	@MemberOrder(sequence = "10")
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	// //////////////////////////////////////
+	// relacion Computadora/Movimiento.
+	// //////////////////////////////////////
+	private Computadora computadora;
+
+	@MemberOrder(sequence = "100")
+	@javax.jdo.annotations.Column(allowsNull = "true")
 	public Computadora getComputadora() {
 		return computadora;
 	}
+	
 
-	public void setComputadora(final Computadora computadora) {
+	public void setComputadora(Computadora computadora) {
 		this.computadora = computadora;
 	}
-		
-	// //////////////////////////////////////
-	// Tecnico (propiedad)
-	// //////////////////////////////////////
 
-	public Tecnico tecnico;
+	public void clearComputadora() {
+		// TODO Auto-generated method stub
 
-	@javax.jdo.annotations.Column(allowsNull = "false")
-	@DescribedAs("Tecnico previamente cargado:")
-	@MemberOrder(sequence = "10")
+	}
+
+	// //////////////////////////////////////
+	// relacion Tecnico/Movimiento.
+	// //////////////////////////////////////
+	// {{ Tecnico (property)
+	private Tecnico tecnico;
+
+	@Optional
+	@MemberOrder(sequence = "1")
+	@Column(allowsNull = "True")
 	public Tecnico getTecnico() {
 		return tecnico;
 	}
-
+	
 	public void setTecnico(final Tecnico tecnico) {
 		this.tecnico = tecnico;
 	}
-		
-	// //////////////////////////////////////
-	// IngresadoPor (propiedad)
-	// //////////////////////////////////////
+//	}
 
-	public String ingresadoPor;
-
-	@javax.jdo.annotations.Column(allowsNull = "false")
-	@DescribedAs("Tecnico que ingresa la computadora:")
-	@MemberOrder(sequence = "10")
-	public String getIngresadoPor() {
-		return ingresadoPor;
+	@Override
+	public int compareTo(final Movimiento movimiento) {
+		return ObjectContracts.compare(this, movimiento, "tecnico.getApellido(),computadora.getIp()");
 	}
 
-	public void setIngresadoPor(final String ingresadoPor) {
-		this.ingresadoPor = ingresadoPor;
-	}
+	 
+	 //////////////////////////////////////
+	 //Injected Services
+	 //////////////////////////////////////
+
+	@SuppressWarnings("unused")
+	@javax.inject.Inject
+	private DomainObjectContainer container;
+	@SuppressWarnings("unused")
+	@javax.inject.Inject
+	private TecnicoRepositorio tecnicoRepositorio;
+	@SuppressWarnings("unused")
+	@javax.inject.Inject
+	private ComputadoraRepositorio computadoraRepositorio;
+
 }
