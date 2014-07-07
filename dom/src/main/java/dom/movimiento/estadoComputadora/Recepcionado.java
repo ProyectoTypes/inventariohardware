6ec5ceaeaa399322456cbccebcc7bda0871e7085
munozdaniel <cipoleto@gmail.com>
2014-07-06 21:12:50 -0300

    Estado Recepcionado: Implementacion de los metodos. - Ademas se agrego el metodo toString.

--- dom/src/main/java/dom/movimiento/estadoComputadora/Recepcionado.java
+++ dom/src/main/java/dom/movimiento/estadoComputadora/Recepcionado.java
@@ -1,5 +1,7 @@
 package dom.movimiento.estadoComputadora;
 
+import org.apache.isis.applib.DomainObjectContainer;
+
 import dom.movimiento.Movimiento;
 
 public class Recepcionado implements IEstado {
@@ -13,16 +15,24 @@ public class Recepcionado implements IEstado {
 	@Override
 	public void equipoRecibido() {
 		// TODO Auto-generated method stub
+		this.movimiento.setEstado(movimiento.getReparando());
 	}
 	
 	@Override
 	public void equipoReparado() {
+		this.container.warnUser("Recepcionado.java - reparando(): El equipo NO ha sido recepcionado");
 		
 	}
 	@Override
 	public void equipoFinalizado() {
+		this.container.warnUser("Recepcionado.java - finalizado(): El equipo NO ha sido Recepcionado/Reparado");
 		
 	}
+	@javax.inject.Inject
+	private DomainObjectContainer container;
 	
+	public String toString() {
+		return "RECIBIENDO EQUIPO";
+	}
 
 }
