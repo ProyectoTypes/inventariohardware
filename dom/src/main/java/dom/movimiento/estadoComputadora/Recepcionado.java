30609919271ca1d0f1ecdc05022647afc23c27b4
munozdaniel <cipoleto@gmail.com>
2014-07-06 21:10:12 -0300

    Estado Recepcionado: implementacion de IEstado - Se agregaron los metodos que pertenecen a la interface.

--- dom/src/main/java/dom/movimiento/estadoComputadora/Recepcionado.java
+++ dom/src/main/java/dom/movimiento/estadoComputadora/Recepcionado.java
@@ -1,20 +1,21 @@
 package dom.movimiento.estadoComputadora;
 
-import org.apache.isis.applib.DomainObjectContainer;
 
-import dom.movimiento.Movimiento;
-
-public class Recepcionado implements IEstado{
+public class Recepcionado implements IEstado {
 
 	@Override
-	public String ManejoDelEstado(Movimiento movimiento) {
+	public void equipoRecibido() {
 		// TODO Auto-generated method stub
-//		movimiento.estado = new Reparando();
-		this.container.informUser("EQUIPO RECEPCIONADO");
-		return "retorno";
 	}
-	@javax.inject.Inject
-	private DomainObjectContainer container;
-	
+
+	@Override
+	public void equipoReparado() {
+
+	}
+
+	@Override
+	public void equipoFinalizado() {
+
+	}
 
 }
