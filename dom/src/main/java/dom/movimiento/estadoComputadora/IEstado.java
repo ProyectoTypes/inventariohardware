d6cf7a51159c532a8bc0c188b50f0dae79e0d6aa
munozdaniel <cipoleto@gmail.com>
2014-07-06 21:08:08 -0300

    IEstado: nuevos metodos.

--- dom/src/main/java/dom/movimiento/estadoComputadora/IEstado.java
+++ dom/src/main/java/dom/movimiento/estadoComputadora/IEstado.java
@@ -1,8 +1,8 @@
 package dom.movimiento.estadoComputadora;
 
-import dom.movimiento.Movimiento;
 
 public interface IEstado {
-	String ManejoDelEstado(Movimiento movimiento);
-
+	public void equipoRecibido();
+	public void equipoReparado();
+	public void equipoFinalizado();
 }
