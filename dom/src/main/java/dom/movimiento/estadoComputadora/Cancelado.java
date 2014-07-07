1619696f10541a5571a44be44b647a0685f62605
munozdaniel <cipoleto@gmail.com>
2014-07-06 21:17:02 -0300

    Estado Cancelado: Implementacion de metodos. - Implementacion de los metodos de la interface. - nuevo Constructor. - nuevo metodo toString - nuevo atributo movimiento.

--- dom/src/main/java/dom/movimiento/estadoComputadora/Cancelado.java
+++ dom/src/main/java/dom/movimiento/estadoComputadora/Cancelado.java
@@ -1,5 +1,37 @@
 package dom.movimiento.estadoComputadora;
 
-public class Cancelado {
+import org.apache.isis.applib.DomainObjectContainer;
+
+import dom.movimiento.Movimiento;
+
+public class Cancelado implements IEstado{
+	Movimiento movimiento;
+	public Cancelado(Movimiento movimiento)
+	{
+		this.movimiento=movimiento;
+	}
+	@Override
+	public void equipoRecibido() {
+		this.container.warnUser("Recepcionado.java - equipoRecibido(): El equipo NO ha sido Reparado/Entregado");
+		
+	}
+
+	@Override
+	public void equipoReparado() {
+		this.container.warnUser("Recepcionado.java - equipoReparado(): El equipo NO ha sido Recepcionado/Finalizado");
+		
+	}
+
+	@Override
+	public void equipoFinalizado() {
+		this.container.warnUser("Cancelado.java - equipoFinalizado(): FIN :) ");
+		
+	}
+	@javax.inject.Inject
+	private DomainObjectContainer container;
+	
+	public String toString() {
+		return "EQUIPO CANCELADO";
+	}
 
 }
