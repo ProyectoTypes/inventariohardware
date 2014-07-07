201e60dbb2dfb9945ed6c2455686c6bcb670baec
munozdaniel <cipoleto@gmail.com>
2014-07-07 19:10:32 -0300

    Tecnico: Agregado operacion aumentar cant. comp.
    - Tambien se agrego la operacion disminuir cantidad computadora.

--- dom/src/main/java/dom/tecnico/Tecnico.java
+++ dom/src/main/java/dom/tecnico/Tecnico.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.VersionStrategy;
+import javax.validation.constraints.Max;
 
 import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.Audited;
@@ -20,53 +21,34 @@ import org.apache.isis.applib.util.ObjectContracts;
 import dom.movimiento.Movimiento;
 import dom.persona.Persona;
 
-@javax.jdo.annotations.PersistenceCapable(identityType=IdentityType.DATASTORE)
-@javax.jdo.annotations.DatastoreIdentity(
-        strategy=javax.jdo.annotations.IdGeneratorStrategy.IDENTITY,
-         column="id")
-@javax.jdo.annotations.Version(
-        strategy=VersionStrategy.VERSION_NUMBER, 
-        column="version")
-@javax.jdo.annotations.Uniques({
-    @javax.jdo.annotations.Unique(
-            name="Tecnico_apellido_must_be_unique", 
-            members={"creadoPor","apellido"})
-})
-@javax.jdo.annotations.Queries( {
-    @javax.jdo.annotations.Query(
-            name = "autoCompletarPorApellido", language = "JDOQL",
-            value = "SELECT "
-                    + "FROM dom.tecnico.Tecnico "
-                    + "WHERE creadoPor == :creadoPor && "
-                    + "apellido.indexOf(:apellido) >= 0"),
-    @javax.jdo.annotations.Query(
-            name = "eliminarTecnicoFalse", language = "JDOQL",
-            value = "SELECT "
-                    + "FROM dom.tecnico.Tecnico "
-                    + "WHERE creadoPor == :creadoPor "
-                    + "   && habilitado == false"),
-    @javax.jdo.annotations.Query(
-            name = "eliminarTecnicoTrue", language = "JDOQL",
-            value = "SELECT "
-                    + "FROM dom.tecnico.Tecnico "
-                    + "WHERE creadoPor == :creadoPor "
-                    + "   && habilitado == true"),
-    @javax.jdo.annotations.Query(
-    		name = "buscarPorApellido", language = "JDOQL", 
-            value = "SELECT "
-                    + "FROM dom.tecnio.Tecnico"
-                    + "WHERE creadoPor == :creadoPor && "
-                    + "apellido.indexOf(:apellido) >= 0"),
-	@javax.jdo.annotations.Query(
-			name = "getTecnico", language = "JDOQL", 
-			value = "SELECT FROM dom.tecnico.Tecnico WHERE creadoPor == :creadoPor")
-})
-
+@javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "id")
+@javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
+@javax.jdo.annotations.Uniques({ @javax.jdo.annotations.Unique(name = "Tecnico_apellido_must_be_unique", members = {
+		"creadoPor", "apellido" }) })
+@javax.jdo.annotations.Queries({
+		@javax.jdo.annotations.Query(name = "autoCompletarPorApellido", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.tecnico.Tecnico "
+				+ "WHERE creadoPor == :creadoPor && "
+				+ "apellido.indexOf(:apellido) >= 0"),
+		@javax.jdo.annotations.Query(name = "eliminarTecnicoFalse", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.tecnico.Tecnico "
+				+ "WHERE creadoPor == :creadoPor "
+				+ "   && habilitado == false"),
+		@javax.jdo.annotations.Query(name = "eliminarTecnicoTrue", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.tecnico.Tecnico "
+				+ "WHERE creadoPor == :creadoPor " + "   && habilitado == true"),
+		@javax.jdo.annotations.Query(name = "buscarPorApellido", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.tecnio.Tecnico"
+				+ "WHERE creadoPor == :creadoPor && "
+				+ "apellido.indexOf(:apellido) >= 0"),
+		@javax.jdo.annotations.Query(name = "getTecnico", language = "JDOQL", value = "SELECT FROM dom.tecnico.Tecnico WHERE creadoPor == :creadoPor") })
 @ObjectType("TECNICO")
 @Audited
-@AutoComplete(repository=TecnicoRepositorio.class, action="autoComplete") //
+@AutoComplete(repository = TecnicoRepositorio.class, action = "autoComplete")
+//
 @Bookmarkable
-public class Tecnico extends Persona implements Comparable<Persona>{
+public class Tecnico extends Persona implements Comparable<Persona> {
 	// //////////////////////////////////////
 	// Identification in the UI
 	// //////////////////////////////////////
@@ -78,44 +60,57 @@ public class Tecnico extends Persona implements Comparable<Persona>{
 	public String iconName() {
 		return "Tecnico";
 	}
-	
+
 	// //////////////////////////////////////
 	// Borrar Usuario
 	// //////////////////////////////////////
 	/**
-    * Método que utilizo para deshabilitar un Tecnico.
-    * 
-    * @return la propiedad habilitado en false.
-    */
+	 * Método que utilizo para deshabilitar un Tecnico.
+	 * 
+	 * @return la propiedad habilitado en false.
+	 */
 	@Named("Eliminar")
 	@PublishedAction
 	@Bulk
-	@MemberOrder(name="accionEliminar", sequence = "1")	
+	@MemberOrder(name = "accionEliminar", sequence = "1")
 	public List<Tecnico> eliminar() {
-		if(getEstaHabilitado()==true){
-				setHabilitado(false);    
-			    container.isPersistent(this);     
-			    container.warnUser("Eliminado " + container.titleOf(this));	
+		if (getEstaHabilitado() == true) {
+			setHabilitado(false);
+			container.isPersistent(this);
+			container.warnUser("Eliminado " + container.titleOf(this));
 		}
 		return null;
 	}
 
-	//Campo que diferencia a tecnico de usuario. El valor por el momento se hara manualmente
+	// Campo que diferencia a tecnico de usuario. El valor por el momento se
+	// hara manualmente
 	// pero lo ideal es que cambie automaticamente segun el patron State.
 	private BigDecimal cantidadComputadora;
-	
+@Max(5)
 	@javax.jdo.annotations.Column(allowsNull = "true")
 	public BigDecimal getCantidadComputadora() {
 		return cantidadComputadora;
 	}
-	
+
 	public void setCantidadComputadora(BigDecimal cantidadComputadora) {
 		this.cantidadComputadora = cantidadComputadora;
 	}
 	
+
+	public void sumaComputadora() {
+		BigDecimal valor = new BigDecimal(1);
+		this.setCantidadComputadora(this.cantidadComputadora.add(valor));
+	}
+
+	public void restaComputadora() {
+		BigDecimal valor = new BigDecimal(-1);
+		this.setCantidadComputadora(this.cantidadComputadora.add(valor));
+
+	}
+
 	// {{ Movimiento (property)
 	private Movimiento movimiento;
-	
+
 	@MemberOrder(sequence = "200")
 	@javax.jdo.annotations.Column(allowsNull = "true")
 	public Movimiento getMovimiento() {
@@ -125,25 +120,25 @@ public class Tecnico extends Persona implements Comparable<Persona>{
 	public void setMovimiento(final Movimiento movimiento) {
 		this.movimiento = movimiento;
 	}
+
 	// }}
 
-
-    // //////////////////////////////////////
-    // CompareTo
-    // //////////////////////////////////////
+	// //////////////////////////////////////
+	// CompareTo
+	// //////////////////////////////////////
 	/**
-	* Implementa Comparable<Tecnico>
-	* Necesario para ordenar por apellido la clase Tecnico.
-	*/
-    @Override
-    public int compareTo(final Persona persona) {
-        return ObjectContracts.compare(this,persona, "apellido");
-    }
-    // //////////////////////////////////////
-    // Injected Services
-    // //////////////////////////////////////
+	 * Implementa Comparable<Tecnico> Necesario para ordenar por apellido la
+	 * clase Tecnico.
+	 */
+	@Override
+	public int compareTo(final Persona persona) {
+		return ObjectContracts.compare(this, persona, "apellido");
+	}
 
+	// //////////////////////////////////////
+	// Injected Services
+	// //////////////////////////////////////
 
 	@javax.inject.Inject
-    private DomainObjectContainer container;
+	private DomainObjectContainer container;
 }
