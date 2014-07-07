514708bbe797c18117b5996aa6bb852fd49e940e
munozdaniel <cipoleto@gmail.com>
2014-07-06 21:11:55 -0300

    Estado Recepcionado: Agregado constructor. - Se agrego nuevo atributo. - Se agrego constructor.

--- dom/src/main/java/dom/movimiento/estadoComputadora/Recepcionado.java
+++ dom/src/main/java/dom/movimiento/estadoComputadora/Recepcionado.java
@@ -1,21 +1,28 @@
 package dom.movimiento.estadoComputadora;
 
+import dom.movimiento.Movimiento;
 
 public class Recepcionado implements IEstado {
+	Movimiento movimiento;
+	public Recepcionado(Movimiento movimiento)
+	{
+		this.movimiento=movimiento;
+	}
 
+	
 	@Override
 	public void equipoRecibido() {
 		// TODO Auto-generated method stub
 	}
-
+	
 	@Override
 	public void equipoReparado() {
-
+		
 	}
-
 	@Override
 	public void equipoFinalizado() {
-
+		
 	}
+	
 
 }
