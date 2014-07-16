493c5b0925c05a87c7d12c4757d6de3a914a94ea
munozdaniel <cipoleto@gmail.com>
2014-07-15 21:14:22 -0300

    Recepcionado: Optimizado met. equipoRecibido. - Control de la cantidad de computadoras de un Tecnico.

--- dom/src/main/java/dom/movimiento/estadoComputadora/Recepcionado.java
+++ dom/src/main/java/dom/movimiento/estadoComputadora/Recepcionado.java
@@ -6,33 +6,40 @@ import dom.movimiento.Movimiento;
 
 public class Recepcionado implements IEstado {
 	Movimiento movimiento;
-	public Recepcionado(Movimiento movimiento)
-	{
-		this.movimiento=movimiento;
+
+	public Recepcionado(Movimiento movimiento) {
+		this.movimiento = movimiento;
 	}
 
-	
 	@Override
 	public void equipoRecibido() {
-		// TODO Auto-generated method stub
-		this.movimiento.getTecnico().sumaComputadora();
-		this.movimiento.setEstado(movimiento.getReparando());
-		
+		if ((this.movimiento.getTecnico().estaDisponible())) {// 0:Ok
+			this.movimiento.getTecnico().sumaComputadora();
+			this.movimiento.setEstado(movimiento.getReparando());
+		} else {
+			this.movimiento.setTecnico(null);
+			this.container
+					.informUser("El Tecnico seleccionado no esta disponible.");
+		}
 	}
-	
+
 	@Override
 	public void equipoReparado() {
-		this.container.warnUser("Recepcionado.java - reparando(): El equipo NO ha sido recepcionado");
-		
+		this.container
+				.warnUser("Recepcionado.java - reparando(): El equipo NO ha sido recepcionado");
+
 	}
+
 	@Override
 	public void equipoFinalizado() {
-		this.container.warnUser("Recepcionado.java - finalizado(): El equipo NO ha sido Recepcionado/Reparado");
-		
+		this.container
+				.warnUser("Recepcionado.java - finalizado(): El equipo NO ha sido Recepcionado/Reparado");
+
 	}
+
 	@javax.inject.Inject
 	private DomainObjectContainer container;
-	
+
 	public String toString() {
 		return "EQUIPO RECIBIDO";
 	}
