5b8b31512a65918b09b484e9de0891edf9ab4adf
Oscar Eduardo Sepulveda <oscarsepulveda85@gmail.com>
2014-07-17 20:47:04 -0300

    Agregando iconName en InsumosRepositorio

--- dom/src/main/java/dom/imsumos/InsumosRepositorio.java
+++ dom/src/main/java/dom/imsumos/InsumosRepositorio.java
@@ -2,4 +2,15 @@ package dom.imsumos;
 
 public class InsumosRepositorio {
 
+	// //////////////////////////////////////
+	// Icono
+	// //////////////////////////////////////
+
+	public String title() {
+		return "insumos";
+	}
+
+	public String iconName() {
+		return "Insumos";
+	}
 }
