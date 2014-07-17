34e60be04b480a0ff9a82b63acf6058f6c3f437c
Oscar Eduardo Sepulveda <oscarsepulveda85@gmail.com>
2014-07-17 20:37:45 -0300

    Agregando atributos a clase Insumos

--- dom/src/main/java/dom/imsumos/Insumos.java
+++ dom/src/main/java/dom/imsumos/Insumos.java
@@ -3,6 +3,14 @@ package dom.imsumos;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.VersionStrategy;
 
+import org.apache.isis.applib.annotation.Audited;
+import org.apache.isis.applib.annotation.AutoComplete;
+import org.apache.isis.applib.annotation.Bookmarkable;
+import org.apache.isis.applib.annotation.DescribedAs;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ObjectType;
+
 @javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
 @javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "id")
 @javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
@@ -25,7 +33,106 @@ import javax.jdo.annotations.VersionStrategy;
 				+ "FROM dom.insumo.Insumo"
 				+ "WHERE creadoPor == :creadoPor "
 				+ "   && codigo.indexOf(:codigo) >= 0"), })
-
+@ObjectType("INSUMOS")
+@Audited
+@AutoComplete(repository = InsumosRepositorio.class, action = "autoComplete")
+@Bookmarkable
 public class Insumos {
+
+	// //////////////////////////////////////
+	// codigo (Atributo)
+	// //////////////////////////////////////
+	private String codigo;
+
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	@DescribedAs("Codigo numero de insumo:")
+	@MemberOrder(sequence = "10")
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	// //////////////////////////////////////
+	// cantidad (Atributo)
+	// //////////////////////////////////////
+	private int cantidad;
+
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	@DescribedAs("Cantidades a solicitar:")
+	@MemberOrder(sequence = "20")
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	// //////////////////////////////////////
+	// producto (Atributo)
+	// //////////////////////////////////////
+	private String producto;
+
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	@DescribedAs("Nombre del insumo:")
+	@MemberOrder(sequence = "30")
+	public String getProducto() {
+		return producto;
+	}
+
+	public void setProducto(String producto) {
+		this.producto = producto;
+	}
+
+	// //////////////////////////////////////
+	// marca (Atributo)
+	// //////////////////////////////////////
+	private String marca;
+
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	@DescribedAs("Marca del Insumo:")
+	@MemberOrder(sequence = "40")
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	// //////////////////////////////////////
+	// observaciones (Atributo)
+	// //////////////////////////////////////
+	private String observaciones;
+
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	@DescribedAs("Observaciones del insumo:")
+	@MemberOrder(sequence = "50")
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	// //////////////////////////////////////
+	// Habilitado (propiedad)
+	// //////////////////////////////////////
+
+	public boolean habilitado;
+
+	@Hidden
+	@MemberOrder(sequence = "60")
+	public boolean getEstaHabilitado() {
+		return habilitado;
+	}
+
+	public void setHabilitado(final boolean habilitado) {
+		this.habilitado = habilitado;
+	}
 
 }
