2e1d5adc35a1d51736b566761e9122ff85dabaf6
munozdaniel <cipoleto@gmail.com>
2014-08-12 20:18:42 -0300

    Recepcionado: Implementacion del met. asignarTecnico.

--- dom/src/main/java/dom/movimiento/equipo/Recepcionado.java
+++ dom/src/main/java/dom/movimiento/equipo/Recepcionado.java
@@ -27,12 +27,12 @@ public class Recepcionado implements IEstado {
 	public String iconName() {
 		return "sector";
 	}
+
 	// {{ Movimiento (property)
 	private Movimiento movimiento;
 
 	@MemberOrder(sequence = "1")
 	@javax.jdo.annotations.Column(allowsNull = "true")
-
 	public Movimiento getMovimiento() {
 		return movimiento;
 	}
@@ -40,40 +40,49 @@ public class Recepcionado implements IEstado {
 	public void setMovimiento(final Movimiento movimiento) {
 		this.movimiento = movimiento;
 	}
+
 	// }}
-	public Recepcionado(Movimiento unMovimiento)
-	{
-		this.movimiento= unMovimiento;
+	public Recepcionado(Movimiento unMovimiento) {
+		this.movimiento = unMovimiento;
 	}
 
 	@Override
 	public void asignarTecnico() {
-		this.getMovimiento().setEstadoActual("CAMBIA DE ESTADO A REPARANDO - EN REPARACION.");
-		this.getMovimiento().getTecnico().sumaComputadora();
-		this.getMovimiento().setEstado(this.getMovimiento().getReparando());
+		this.getMovimiento().setEstadoActual(
+				"CAMBIA DE ESTADO A REPARANDO - EN REPARACION.");
+		if (this.getMovimiento().getTecnico().estaDisponible()) {
+			this.getMovimiento().getTecnico().sumaComputadora();
+			this.getMovimiento().setEstado(this.getMovimiento().getReparando());
+		} else {
+			this.getMovimiento().setTecnico(null);
+			this.getMovimiento().clearComputadora();
+		}
 		// this.container
 		// .informUser("El Tecnico seleccionado no esta disponible.");
 
 	}
 
 	@Override
-	public void esperarRepuestos( ) {
-		this.getMovimiento().setEstadoActual("NO ESPERA: NO ES EL ESTADO REPARANDO.");
+	public void esperarRepuestos() {
+		this.getMovimiento().setEstadoActual(
+				"NO ESPERA: NO ES EL ESTADO REPARANDO.");
 	}
 
 	@Override
 	public void finalizarSoporte() {
-		this.getMovimiento().setEstadoActual("NO FINALIZA: NO ES EL ESTADO REPARANDO.");
+		this.getMovimiento().setEstadoActual(
+				"NO FINALIZA: NO ES EL ESTADO REPARANDO.");
 	}
 
 	@Override
-	public void noHayRepuestos( ) {
+	public void noHayRepuestos() {
 		// TODO Auto-generated method stub
-		this.getMovimiento().setEstadoActual("NO CANCELA: NO ES EL ESTADO ESPERANDO");
+		this.getMovimiento().setEstadoActual(
+				"NO CANCELA: NO ES EL ESTADO ESPERANDO");
 	}
 
 	@Override
-	public void llegaronRepuestos( ) {
+	public void llegaronRepuestos() {
 		this.getMovimiento().setEstadoActual("NO ES EL ESTADO ESPERANDO");
 	}
 
