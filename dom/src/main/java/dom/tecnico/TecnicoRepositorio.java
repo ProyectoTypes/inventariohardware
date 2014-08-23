7f53bee0917ca003c18b187e472a96bbf4b8b901
Exequiel Wiedermann <EWFWiedermann@gmail.com>
2014-08-23 15:02:10 -0300

    TecnicoRepositorio: quitando la metanotacion @optional de email

--- dom/src/main/java/dom/tecnico/TecnicoRepositorio.java
+++ dom/src/main/java/dom/tecnico/TecnicoRepositorio.java
@@ -65,7 +65,7 @@ public class TecnicoRepositorio {
 	public Tecnico addTecnico(final @Optional Sector sector,
 			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Apellido") String apellido,
 			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Nombre") String nombre,
-			final @Optional @RegEx(validation = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$") @Named("E-mail") String email			) {
+			final @RegEx(validation = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$") @Named("E-mail") String email			) {
 		
 		return nuevoTecnico(apellido, nombre, email,sector, this.currentUserName());
 	}
