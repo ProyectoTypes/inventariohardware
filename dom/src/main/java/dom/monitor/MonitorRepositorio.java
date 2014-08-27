6b2db635ce26255b10368c54be9b022fc012d8f1
Exequiel Wiedermann <EWFWiedermann@gmail.com>
2014-08-26 21:32:13 -0300

    Agregando las propiedades de la clase MonitorRepositorio

--- dom/src/main/java/dom/monitor/MonitorRepositorio.java
+++ dom/src/main/java/dom/monitor/MonitorRepositorio.java
@@ -1,5 +1,49 @@
+/*
+ * This is a software made for inventory control
+ * 
+ * Copyright (C) 2014, ProyectoTypes
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * 
+ * 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 package dom.monitor;
 
+import java.util.List;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Optional;
+import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.query.QueryDefault;
+
+@Named("Monitor")
+
 public class MonitorRepositorio {
+	// //////////////////////////////////////
+	// Icono
+	// //////////////////////////////////////
+
+	public String title() {
+		return "Monitor";
+	}
+
+	public String iconName() {
+		return "Monitor";
+	}
+	
 
 }
