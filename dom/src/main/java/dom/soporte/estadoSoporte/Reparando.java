aaba409a4505a4ce210237f15c15aa9a91e2698a
munozdaniel <cipoleto@gmail.com>
2014-09-02 23:40:51 -0300

    Reparando: Optimizando solicitando insumos.

--- dom/src/main/java/dom/soporte/estadoSoporte/Reparando.java
+++ dom/src/main/java/dom/soporte/estadoSoporte/Reparando.java
@@ -1,5 +1,6 @@
 package dom.soporte.estadoSoporte;
 
+import javax.inject.Inject;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.VersionStrategy;
 
@@ -10,6 +11,8 @@ import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.ObjectType;
 
 import servicio.email.EmailService;
+import dom.insumo.Insumo;
+import dom.insumo.InsumoRepositorio;
 import dom.soporte.Soporte;
 import dom.tecnico.Tecnico;
 
@@ -21,6 +24,7 @@ import dom.tecnico.Tecnico;
 @Audited
 @Bookmarkable
 public class Reparando implements IEstado {
+
 	public String title() {
 		return "REPARANDO ";
 	}
@@ -42,13 +46,18 @@ public class Reparando implements IEstado {
 		return soporte;
 	}
 
+	@SuppressWarnings("unused")
+	private void setSoporte(final Soporte soporte) {
+		this.soporte = soporte;
+	}
+
 	// }}
 	/**
 	 * Permite cambiar el Tecnico encargado del Soporte Tecnico.
 	 * <p>
-	 * Chequea que el nuevo Tecnico este Disponible, en caso afirmativo,
-	 * desvincula el Tecnico anterior de la Computadora y del Soporte, y vincula
-	 * el Tecnico nuevo al Soporte.
+	 * Chequea que el nuevo Tecnico este Disponible, en caso afirmativo, de ser
+	 * necesario desvincula el Tecnico anterior de la Computadora y del Soporte.
+	 * Finalmente vincula el Tecnico nuevo al Soporte.
 	 * </p>
 	 * <p>
 	 * Se mantiene en el estado Reparando
@@ -58,35 +67,50 @@ public class Reparando implements IEstado {
 	 */
 	@Override
 	public void asignarTecnico(final Tecnico tecnico) {
-		if (this.getSoporte().getTecnico() != null) {
-			this.getSoporte().getTecnico()
-					.desvincularComputadora(this.getSoporte().getComputadora());
-			this.getSoporte().setTecnico(null);
-		}
+
 		if (this.getSoporte().getTecnico().estaDisponible()) {
+			if (this.getSoporte().getTecnico() != null) {
+				this.getSoporte()
+						.getTecnico()
+						.desvincularComputadora(
+								this.getSoporte().getComputadora());
+				this.getSoporte().setTecnico(null);
+			}
 			this.getSoporte().getTecnico().sumaComputadora();
 			this.getSoporte().getTecnico()
 					.addToComputadora(this.getSoporte().getComputadora());
-			this.getSoporte().setEstado(this.getSoporte().getReparando());
+
 			this.container.informUser("ASIGNADO NUEVO TECNICO.");
 		} else {
 			this.getSoporte().setTecnico(null);
 			this.container
-					.informUser("El Tecnico seleccionado no esta disponible.");
+					.informUser("EL TECNICO SELECCIONADO NO SE ENCUENTRA DISPONIBLE.");
 		}
 
 	}
 
+	/**
+	 * Se persiste el Insumo solicitado, luego es vinculado al Soporte.
+	 * <p>
+	 * Cambio de Estado: Reparando -> Esperando
+	 * </p>
+	 */
 	@Override
-	public void solicitarInsumos() {
+	public void solicitarInsumos(final String codigo, final int cantidad,
+			final String producto, final String marca,
+			final String observaciones) {
+		Insumo unInsumo = this.insumoRepositorio.addInsumo(codigo, cantidad,
+				producto, marca, observaciones);
+		this.getSoporte().agregarUnInsumo(unInsumo);
 		this.getSoporte().setEstado(this.getSoporte().getEsperando());
-		this.container.informUser("ESPERANDO QUE LOS REPUESTOS LLEGUEN.");
+		this.container.informUser("EN ESPERA DE LOS INSUMOS SOLICITADOS.");
 	}
 
 	@Override
 	public void finalizarSoporte() {
 		emailService.send(this.getSoporte().getComputadora());
-		this.getSoporte().getTecnico().desvincularComputadora();
+		this.getSoporte().getTecnico()
+				.desvincularComputadora(this.getSoporte().getComputadora());
 		this.getSoporte().setEstado(this.getSoporte().getEntregando());
 		this.container.informUser("SOPORTE TECNICO FINALIZADO.");
 	}
@@ -95,18 +119,20 @@ public class Reparando implements IEstado {
 	private EmailService emailService;
 
 	@Override
-	public void noHayRepuestos() {
+	public void noHayInsumos() {
 		this.container.informUser("EL EQUIPO CONTINUA EN REPARACION.");
 
 	}
 
 	@Override
-	public void llegaronRepuestos() {
+	public void llegaronInsumos() {
 		this.container.informUser("ES NECESARIO SOLICITAR REPUESTOS.");
 	}
 
-	@javax.inject.Inject
+	@Inject
 	private DomainObjectContainer container;
+	@Inject
+	private InsumoRepositorio insumoRepositorio;
 
 	@Override
 	public void asignarEquipo() {
