cbaa4706d75b50693b14cd0f99a919d9ba153b59
munozdaniel <munozda87@hotmail.com>
2014-09-21 16:33:46 -0300

    Permiso: agregado atributos.

--- dom/src/main/java/dom/permiso/Permiso.java
+++ dom/src/main/java/dom/permiso/Permiso.java
@@ -1,5 +1,53 @@
 package dom.permiso;
 
-public class Permiso {
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.apache.isis.applib.annotation.Bounded;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ObjectType;
+
+@javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "id")
+@javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
+@ObjectType("Permiso")
+@Bounded
+public class Permiso implements Comparable<Permiso> {
+
+	public String title() {
+		return nombre;
+	}
+
+	private String nombre;
+
+	@MemberOrder(sequence = "1")
+	@Column(allowsNull = "false")
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(final String detalles) {
+		this.nombre = detalles;
+	}
+
+	private String path;
+
+	@MemberOrder(sequence = "1")
+	@Column(allowsNull = "false")
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(final String text) {
+		this.path = text;
+	}
+
+	@Override
+	public int compareTo(Permiso other) {
+		return this.getNombre().compareTo(
+				other.getNombre());
+
+	}
 
 }
--- dom/src/main/java/dom/permiso/PermisoRepositorio.java
+++ dom/src/main/java/dom/permiso/PermisoRepositorio.java
@@ -1,5 +1,60 @@
 package dom.permiso;
 
+import java.util.List;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.ActionSemantics;
+import org.apache.isis.applib.annotation.ActionSemantics.Of;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Named;
+
+@DomainService(menuOrder = "45", repositoryFor = Permiso.class)
+@Named("Permisos")
 public class PermisoRepositorio {
+
+	public String getId() {
+		return "Permiso";
+	}
+
+	public String iconName() {
+		return "Tecnico";
+	}
+
+
+	@ActionSemantics(Of.SAFE)
+	@MemberOrder(sequence = "1")
+	@Named("Todos los Permisos")
+	public List<Permiso> listAll() {
+		return container.allInstances(Permiso.class);
+	}
+
+
+	@MemberOrder(sequence = "2")
+	@Named("Nuevo Permiso")
+	public Permiso create(
+			final @Named("Detalles") String detalles,
+			final @Named("Text") String text) {
+		final Permiso permiso = container.newTransientInstance(Permiso.class);
+
+		permiso.setNombre(detalles);
+		permiso.setPath(text);
+
+		container.persistIfNotAlready(permiso);
+		return permiso;
+	}
+
+
+	@ActionSemantics(Of.NON_IDEMPOTENT)
+	@MemberOrder(sequence = "4")
+	@Named("Eliminar Permiso")
+	public String removePermission(@Named("Permiso") Permiso permiso) {
+		String permissionDescription = permiso.getNombre();
+		container.remove(permiso);
+		return "El Permiso: " + permissionDescription + " ha sido eliminado";
+	}
+
+	@javax.inject.Inject
+	DomainObjectContainer container;
 
 }
