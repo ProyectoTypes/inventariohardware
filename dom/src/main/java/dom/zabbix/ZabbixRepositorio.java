fbc1cf077376024e89db428898282164d80dd7c2
munozdaniel <munozda87@hotmail.com>
2014-10-23 17:15:01 -0300

    ZabbixRepo: Agregando met. init para generar una ip - por defecto, la localhost: 127.0.0.1

--- dom/src/main/java/dom/zabbix/ZabbixRepositorio.java
+++ dom/src/main/java/dom/zabbix/ZabbixRepositorio.java
@@ -16,25 +16,41 @@ public class ZabbixRepositorio {
 	public Zabbix configurarIpServidor(final @Named("IP") String ip) {
 		return updateZabbix(ip);
 	}
+
 	@Programmatic
-	public Zabbix updateZabbix(final String ip)
-	{
+	public Zabbix updateZabbix(final String ip) {
 		Zabbix obj = obtenerCuentaZabbix();
-		if(obj==null)
+		if (obj == null)
 			return null;
 		obj.setIp(ip);
 		obj.setToken(ZabbixAutenticacion.obtenerTokenPorIp(ip));
 		container.flush();
 		return obj;
 	}
-	
-	public  Zabbix obtenerCuentaZabbix()
-	{
-		Zabbix retorno = container.firstMatch(new QueryDefault<Zabbix>(Zabbix.class, "obtenerCuenta"));
+
+	public Zabbix obtenerCuentaZabbix() {
+		Zabbix retorno = container.firstMatch(new QueryDefault<Zabbix>(
+				Zabbix.class, "obtenerCuenta"));
 		if (retorno == null)
 			container.informUser("No se encontraron registros de Zabbix");
 		return retorno;
 	}
+
+	@Programmatic
+	public Zabbix addZabbix(final String ip) {
+		Zabbix obj = container.newTransientInstance(Zabbix.class);
+		obj.setIp(ip);
+		obj.setToken(ZabbixAutenticacion.obtenerTokenPorIp(ip));
+		container.persistIfNotAlready(obj);
+		return obj;
+	}
+
+	@Programmatic
+	@PostConstruct
+	public void init() {
+		addZabbix("127.0.0.1");
+	}
+
 	@javax.inject.Inject
 	DomainObjectContainer container;
 }
