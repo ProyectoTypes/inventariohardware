9bf77d67881f30e820cab0c2dcccbb2f7962e4ee
munozdaniel <munozda87@hotmail.com>
2014-10-22 21:26:24 -0300

    Agregado clase CpuItem

---
+++ dom/src/main/java/dom/zabbix/monitoreo/item/CpuItem.java
@@ -0,0 +1,29 @@
+package dom.zabbix.monitoreo.item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+
+public class CpuItem extends ItemManager {
+
+	@Override
+	protected void cargarParametros() {
+		Map<String, String> cpu = new HashMap<String, String>();
+		cpu.put("key_", "system.cpu.load");
+		try {
+			this.getParametrosJson().put("search", cpu);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	protected String obtenerValor() throws JSONException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
