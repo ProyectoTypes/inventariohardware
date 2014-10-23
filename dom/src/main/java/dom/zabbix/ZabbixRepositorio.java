57033833a1a4526bf6bae458a656c51944946812
munozdaniel <munozda87@hotmail.com>
2014-10-23 17:13:55 -0300

    Agregando ZabbixRepositorio

---
+++ dom/src/main/java/dom/zabbix/ZabbixRepositorio.java
@@ -0,0 +1,16 @@
+package dom.zabbix;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.query.QueryDefault;
+
+@DomainService(menuOrder = "100")
+@Named("Servidor")
+public class ZabbixRepositorio {
+
+	
+}
