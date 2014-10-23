72d0b44476808f877927721d79f517cc876bd34d
munozdaniel <munozda87@hotmail.com>
2014-10-23 18:34:17 -0300

    Testeando ping

--- dom/src/main/java/dom/zabbix/ZabbixTest.java
+++ dom/src/main/java/dom/zabbix/ZabbixTest.java
@@ -1,27 +1,37 @@
 package dom.zabbix;
 
-import org.json.JSONException;
+import java.io.IOException;
+import java.net.InetAddress;
 
-import dom.zabbix.monitoreo.item.CpuItem;
-import dom.zabbix.monitoreo.item.ItemManager;
-import dom.zabbix.monitoreo.item.RamItem;
+import org.json.JSONException;
 
 public class ZabbixTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
+//		try {
 //			System.out.println("HOST ID "+ host.requestHostId("127.0.0.1"));
-			ItemManager cpu = new CpuItem();
-			System.out.println("CPU "+cpu.requestItemGet("127.0.0.1"));
-//			Alertas alerta = new Alertas();
-//			System.out.println("ALERTAS: "+ alerta.requestAlertGet("127.0.0.1"));
-			RamItem ram = new RamItem();
-			System.out.println("RAM: "+ ram.requestItemGet("127.0.0.1"));
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			System.out.println("ERROR: "+e.getMessage());
-		}
+//			ItemManager cpu = new CpuItem();
+//			System.out.println("CPU "+cpu.requestItemGet("127.0.0.1"));
+////			Alertas alerta = new Alertas();
+////			System.out.println("ALERTAS: "+ alerta.requestAlertGet("127.0.0.1"));
+//			RamItem ram = new RamItem();
+//			System.out.println("RAM: "+ ram.requestItemGet("127.0.0.1"));
+			InetAddress ping; 
+			String ip = "13"; // Ip de la máquina remota 
+			try { 
+			ping = InetAddress.getByName(ip);
+			if(ping.isReachable(5000)){ 
+			System.out.println(ip+" - SI responde!"); 
+			}else { 
+			System.out.println(ip+" - no responde!"); 
+			}
+			} catch (IOException ex) { System.out.println(ex); } 
+			
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			System.out.println("ERROR: "+e.getMessage());
+//		}
 	}
 
 }
