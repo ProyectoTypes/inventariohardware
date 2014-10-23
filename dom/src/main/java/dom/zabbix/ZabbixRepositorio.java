29ec343a7ca8a3280a42bbc5d3c0ea27b37ab2be
munozdaniel <munozda87@hotmail.com>
2014-10-23 17:14:51 -0300

    ZabbixRepo: Agregando met. configurarIp

--- dom/src/main/java/dom/zabbix/ZabbixRepositorio.java
+++ dom/src/main/java/dom/zabbix/ZabbixRepositorio.java
@@ -12,5 +12,29 @@ import org.apache.isis.applib.query.QueryDefault;
 @Named("Servidor")
 public class ZabbixRepositorio {
 
+	@Named("Configurar IP")
+	public Zabbix configurarIpServidor(final @Named("IP") String ip) {
+		return updateZabbix(ip);
+	}
+	@Programmatic
+	public Zabbix updateZabbix(final String ip)
+	{
+		Zabbix obj = obtenerCuentaZabbix();
+		if(obj==null)
+			return null;
+		obj.setIp(ip);
+		obj.setToken(ZabbixAutenticacion.obtenerTokenPorIp(ip));
+		container.flush();
+		return obj;
+	}
 	
+	public  Zabbix obtenerCuentaZabbix()
+	{
+		Zabbix retorno = container.firstMatch(new QueryDefault<Zabbix>(Zabbix.class, "obtenerCuenta"));
+		if (retorno == null)
+			container.informUser("No se encontraron registros de Zabbix");
+		return retorno;
+	}
+	@javax.inject.Inject
+	DomainObjectContainer container;
 }
