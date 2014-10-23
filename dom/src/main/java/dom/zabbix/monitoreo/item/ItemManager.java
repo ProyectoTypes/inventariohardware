5ddbd8badde28c1b0b16edd256f7328136b8f858
munozdaniel <munozda87@hotmail.com>
2014-10-22 21:27:06 -0300

    Agregado clase ItemManager

---
+++ dom/src/main/java/dom/zabbix/monitoreo/item/ItemManager.java
@@ -0,0 +1,37 @@
+package dom.zabbix.monitoreo.item;
+
+import org.json.JSONException;
+
+import dom.zabbix.ZabbixManager;
+
+public abstract class ItemManager extends ZabbixManager {
+
+	public String requestItemGet(final String ip) throws JSONException {
+
+		String token = this.obtenerToken(ip);
+
+		this.getParametrosJson().put("output", "extend");
+		this.getParametrosJson().put("host", HOST);
+
+		this.cargarParametros();
+
+		this.getObjetoJson().put("sortfield", "name");
+		this.getObjetoJson().put("params", this.getParametrosJson());
+		this.getObjetoJson().put("jsonrpc", "2.0");
+		this.getObjetoJson().put("method", "item.get");
+		this.getObjetoJson().put("auth", token);
+		this.getObjetoJson().put("id", "1");
+
+		return this.obtenerValor();
+		
+	}
+
+	/**
+	 * Metodo abtrascto del template method. Cada subclase implementara este
+	 * metodo a su criterio.
+	 */
+	protected abstract void cargarParametros();
+	protected abstract String obtenerValor() throws JSONException;
+
+
+}
