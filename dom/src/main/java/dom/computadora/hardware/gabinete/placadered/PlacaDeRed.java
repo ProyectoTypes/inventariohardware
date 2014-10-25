199575532a9826bba7ce920f63297c70a3188ef4
Oscar Eduardo Sepulveda <oscarsepulveda85@gmail.com>
2014-10-25 09:54:25 -0300

    Ubicando placa de red en paquete gabinete

---
+++ dom/src/main/java/dom/computadora/hardware/gabinete/placadered/PlacaDeRed.java
@@ -0,0 +1,81 @@
+/*
+ * This is a software made for inventory control
+ * 
+ * Copyright (C) 2014, ProyectoTypes
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * 
+ * 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package dom.computadora.hardware.gabinete.placadered;
+
+import org.apache.isis.applib.annotation.Audited;
+import org.apache.isis.applib.annotation.DescribedAs;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ObjectType;
+import org.apache.isis.applib.annotation.RegEx;
+
+@ObjectType("PLACA DE RED")
+@Audited
+public class PlacaDeRed {
+
+	// //////////////////////////////////////
+	// Identificacion en la UI. Aparece como item del menu
+	// //////////////////////////////////////
+
+	public String title() {
+		return getIp();
+	}
+
+	public String iconName() {
+		return "PLACA DE RED";
+	}
+
+	// //////////////////////////////////////
+	// IP placa de red
+	// //////////////////////////////////////
+
+	private String ip;
+
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	@RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*")
+	@DescribedAs("IP de la placa de red")
+	@MemberOrder(sequence = "10")
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(final String ip) {
+		this.ip = ip;
+	}
+
+	// //////////////////////////////////////
+	// MAC de la placa de red
+	// //////////////////////////////////////
+
+	private String mac;
+
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	@RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*")
+	@DescribedAs("MAC de la placa de red")
+	@MemberOrder(sequence = "20")
+	public String getMac() {
+		return mac;
+	}
+
+	public void setMac(final String mac) {
+		this.mac = mac;
+	}
+}
