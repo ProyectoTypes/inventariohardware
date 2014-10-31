84be3eb97ce97e5535ea776cd1cdd84907f57a90
munozdaniel <munozda87@hotmail.com>
2014-10-30 21:53:12 -0300

    ItemManager: Agregado comentarios sobre metodo.

--- dom/src/main/java/dom/zabbix/monitoreo/item/ItemManager.java
+++ dom/src/main/java/dom/zabbix/monitoreo/item/ItemManager.java
@@ -1,17 +1,32 @@
 package dom.zabbix.monitoreo.item;
 
+import javax.annotation.PostConstruct;
+
 import org.json.JSONException;
 
+import dom.zabbix.Zabbix;
 import dom.zabbix.ZabbixManager;
+import dom.zabbix.ZabbixRepositorio;
 
 public abstract class ItemManager extends ZabbixManager {
 
-	public String requestItemGet(final String ip) throws JSONException {
+	
 
-		String token = getZabbix().getToken();
+	/**
+	 * La ip que entra como parametro se refiere a la ip del Host. El metodo
+	 * permite obtener los datos de los items generados en zabbix.
+	 * 
+	 * @param ip
+	 * @return
+	 * @throws JSONException
+	 */
+	@PostConstruct
+	public String requestItemGet(final String ip, final Zabbix zabbix) throws JSONException {
+		System.out.println("RequestItem");
+		String token = zabbix.getToken();// Seteado anteriormente.
 
 		this.getParametrosJson().put("output", "extend");
-		this.getParametrosJson().put("host", getZabbix().getHost());
+		this.getParametrosJson().put("host", zabbix.getHost());
 
 		this.cargarParametros();
 
@@ -23,7 +38,7 @@ public abstract class ItemManager extends ZabbixManager {
 		this.getObjetoJson().put("id", "1");
 
 		return this.obtenerValor(ip);
-		
+
 	}
 
 	/**
@@ -31,7 +46,7 @@ public abstract class ItemManager extends ZabbixManager {
 	 * metodo a su criterio.
 	 */
 	protected abstract void cargarParametros();
-	protected abstract String obtenerValor(String ip) throws JSONException;
 
+	protected abstract String obtenerValor(String ip) throws JSONException;
 
 }
