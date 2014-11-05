440e95b13b16b67a228bb1dce8f744a3f5f520bd
oscarsepulveda <oscarsepulveda85@gmail.com>
2014-11-05 15:02:45 -0300

    TecnicoFixture: modificando metodos sobre escritos

--- fixture/src/main/java/fixture/tecnico/TecnicoFixture.java
+++ fixture/src/main/java/fixture/tecnico/TecnicoFixture.java
@@ -18,40 +18,16 @@
  * 
  * 
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
+ */
 package fixture.tecnico;
 
 import org.apache.isis.applib.fixturescripts.FixtureScript;
 
-import dom.sector.Sector;
-import dom.tecnico.Tecnico;
-import dom.tecnico.TecnicoRepositorio;
+public class TecnicoFixture extends FixtureScript {
 
-public class TecnicoFixture extends FixtureScript  {
-
-	public TecnicoFixture() {
-		withDiscoverability(Discoverability.DISCOVERABLE);
-	}
-
-	// //////////////////////////////////////
-
-	private Tecnico create(final Sector sector,final String apellido, final String nombre, final String email, ExecutionContext executionContext) {
-		return executionContext.add(this, tecnicos.addTecnico(sector, apellido, nombre, email));
-	}
-
-	// //////////////////////////////////////
 	@Override
 	protected void execute(ExecutionContext executionContext) {
-
-		// prereqs
-		execute(new TecnicoFixtureBaja(), executionContext);
-		// create
-		create(null,"Vergara","Nicolas","nicolas.vergara@proyectotypes.com.ar", executionContext);
-		create(null,"Sepulveda","Oscar","oscar.sepulveda@proyectotypes.com.ar", executionContext);
-		create(null,"Munoz","Daniel","daniel.munoz@proyectotypes.com.ar", executionContext);
-		create(null,"Wiedermann","Exequiel","exequie.wiedermann@proyectotypes.com.ar", executionContext);
+		// TODO Auto-generated method stub
+		
 	}
-
-	@javax.inject.Inject
-	private TecnicoRepositorio tecnicos;
 }
