2095403173266de5d5bf9dd969c7d776619756ee
munozdaniel <munozda87@hotmail.com>
2014-11-13 20:17:28 -0300

    Procesador: Implementando met. listar. Agregado query.

--- dom/src/main/java/dom/computadora/hardware/gabinete/procesador/ProcesadorRepositorio.java
+++ dom/src/main/java/dom/computadora/hardware/gabinete/procesador/ProcesadorRepositorio.java
@@ -1,5 +1,7 @@
 package dom.computadora.hardware.gabinete.procesador;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.isis.applib.DomainObjectContainer;
@@ -7,10 +9,11 @@ import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.query.QueryDefault;
 
 @DomainService
 @Named("Procesador")
-@Hidden()
+@Hidden
 public class ProcesadorRepositorio {
 	public ProcesadorRepositorio() {
 
@@ -45,4 +48,9 @@ public class ProcesadorRepositorio {
 
 	@Inject
 	private DomainObjectContainer container;
+
+	public List<Procesador> listar() {
+		return this.container.allMatches(new QueryDefault<Procesador>(
+				Procesador.class, "listar"));
+	}
 }
