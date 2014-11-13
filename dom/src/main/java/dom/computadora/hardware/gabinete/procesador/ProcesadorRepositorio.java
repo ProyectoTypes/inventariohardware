c2869a10cb87efc22e2cabdf7a907e40665f09da
munozdaniel <munozda87@hotmail.com>
2014-11-13 16:32:28 -0300

    ProcesadorRepo: persistiendo una instancia de Procesador.

---
+++ dom/src/main/java/dom/computadora/hardware/gabinete/procesador/ProcesadorRepositorio.java
@@ -0,0 +1,48 @@
+package dom.computadora.hardware.gabinete.procesador;
+
+import javax.inject.Inject;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Named;
+
+@DomainService
+@Named("Procesador")
+@Hidden()
+public class ProcesadorRepositorio {
+	public ProcesadorRepositorio() {
+
+	}
+
+	// //////////////////////////////////////
+	// Icono
+	// //////////////////////////////////////
+
+	public String getId() {
+		return "procesador";
+	}
+
+	public String iconName() {
+		return "procesador";
+	}
+
+	@MemberOrder(sequence = "10")
+	@Named("Agregar Procesador")
+	public Procesador addProcesador(final @Named("Modelo") String modelo) {
+		return nuevoProcesador(modelo);
+	}
+
+	private Procesador nuevoProcesador(final String modelo) {
+
+		Procesador obj = this.container.newTransientInstance(Procesador.class);
+		obj.setModelo(modelo);
+		this.container.persistIfNotAlready(obj);
+		this.container.flush();
+		return obj;
+	}
+
+	@Inject
+	private DomainObjectContainer container;
+}
