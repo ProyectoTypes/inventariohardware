ace6fc7764f9a1e432bee937d1c120375990b5fa
Exequiel Wiedermann <EWFWiedermann@gmail.com>
2014-11-15 14:42:10 -0300

    PlacaDeRed:Eliminando warning innecesarios.

--- dom/src/main/java/dom/computadora/hardware/gabinete/placadered/PlacaDeRed.java
+++ dom/src/main/java/dom/computadora/hardware/gabinete/placadered/PlacaDeRed.java
@@ -27,7 +27,6 @@ import javax.jdo.annotations.VersionStrategy;
 import org.apache.isis.applib.annotation.DescribedAs;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.ObjectType;
-import org.apache.isis.applib.annotation.RegEx;
 
 @javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
 @javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "id")
