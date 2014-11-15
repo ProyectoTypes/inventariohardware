2b9f50e2240b0d2c352ae6f44510209b3708dbb1
munozdaniel <munozda87@hotmail.com>
2014-11-14 21:38:39 -0300

    HardwareRepo: metodo para agregar hardware

--- dom/src/main/java/dom/computadora/hardware/HardwareRepositorio.java
+++ dom/src/main/java/dom/computadora/hardware/HardwareRepositorio.java
@@ -1,8 +1,15 @@
 package dom.computadora.hardware;
 
+import javax.inject.Inject;
+
+import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
+
+import dom.computadora.hardware.gabinete.Gabinete;
+import dom.computadora.hardware.impresora.Impresora;
+import dom.computadora.hardware.monitor.Monitor;
 
 @DomainService
 @Named("Hardware")
@@ -19,4 +26,31 @@ public class HardwareRepositorio {
 		return "Hardware";
 	}
 
+	@MemberOrder(sequence = "10")
+	@Named("Agregar Hardware")
+	public Hardware addHardware(final @Named("Fabricante") String fabricante,
+			final @Named("Monitor") Monitor monitor,
+			final @Named("Gabinete") Gabinete gabinete,
+			final @Named("Impresora") Impresora impresora) {
+
+		return nuevoHardware(fabricante, monitor, gabinete, impresora);
+
+	}
+
+	private Hardware nuevoHardware(final String fabricante,
+			final Monitor monitor, final Gabinete gabinete,
+			final Impresora impresora) {
+
+		Hardware hardware = this.container.newTransientInstance(Hardware.class);
+		hardware.setFabricante(fabricante);
+		hardware.setGabinete(gabinete);
+		hardware.setImpresora(impresora);
+		hardware.setMonitor(monitor);
+		this.container.persistIfNotAlready(hardware);
+		this.container.flush();
+		return hardware;
+	}
+
+	@Inject
+	private DomainObjectContainer container;
 }
