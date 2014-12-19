33cf3679e1c6ea4b1e0902034586c82e2150fbfc
--global <oscarsepulveda85@gmail.com>
2014-12-19 20:43:17 -0300

    HardwareRepositorio: ocultando metodo

--- dom/src/main/java/dom/computadora/hardware/HardwareRepositorio.java
+++ dom/src/main/java/dom/computadora/hardware/HardwareRepositorio.java
@@ -7,6 +7,7 @@ import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.NotContributed;
 
 import dom.computadora.hardware.gabinete.Gabinete;
 import dom.computadora.hardware.impresora.Impresora;
@@ -46,6 +47,7 @@ public class HardwareRepositorio {
 	 * @param impresora 
 	 * @return  hardware
 	 */
+	@NotContributed
 	@MemberOrder(sequence = "10")
 	@Named("Agregar Hardware")
 	public Hardware addHardware(final @Named("Fabricante") String fabricante,
