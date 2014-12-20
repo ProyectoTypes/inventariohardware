164be4a809112aaa446c655d88eeb1fc248e2dbe
NicoVergara <nicolasvergara89@gmail.com>
2014-12-20 11:05:47 -0300

    Sector: corrigiendo imports.

---
+++ dom/src/main/java/dom/sector/Sector.java
@@ -0,0 +1,169 @@
+/*
+ * This is a software made for inventory control
+ * 
+ * Copyright (C) 2014, ProyectoTypes
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * 
+ * 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+package dom.sector;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.AutoComplete;
+import org.apache.isis.applib.annotation.Bulk;
+import org.apache.isis.applib.annotation.DescribedAs;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MemberGroupLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.ObjectType;
+import org.apache.isis.applib.annotation.PublishedAction;
+import org.apache.isis.applib.annotation.RegEx;
+import org.apache.isis.applib.annotation.Where;
+import org.apache.isis.applib.util.ObjectContracts;
+
+@javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "id")
+@javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
+@javax.jdo.annotations.Uniques({ @javax.jdo.annotations.Unique(name = "Sector_nombreSector_must_be_unique", members = {
+		"creadoPor", "nombreSector" }) })
+@javax.jdo.annotations.Queries({
+		@javax.jdo.annotations.Query(name = "autoCompletePorNombreSector", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.sector.Sector "
+				+ "WHERE nombreSector.indexOf(:nombreSector) >= 0"),
+		@javax.jdo.annotations.Query(name = "ListarSectores", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.sector.Sector "),
+		@javax.jdo.annotations.Query(name = "listarHabilitados", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.sector.Sector "
+				+ "WHERE habilitado == true"),
+		@javax.jdo.annotations.Query(name = "buscarPorNombre", language = "JDOQL", value = "SELECT "
+				+ "FROM dom.sector.Sector "
+				+ "WHERE nombreSector.indexOf(:nombreSector) >= 0") })
+@ObjectType("SECTOR")
+@AutoComplete(repository = SectorRepositorio.class, action = "autoComplete")
+@MemberGroupLayout(columnSpans = { 3, 0, 0, 9 })
+public class Sector implements Comparable<Sector> {
+
+	// //////////////////////////////////////
+	// Identificacion en la UI
+	// //////////////////////////////////////
+
+	public String title() {
+		return this.getNombreSector();
+	}
+
+	public String iconName() {
+		return "Sector";
+	}
+
+	// //////////////////////////////////////
+	// Nombre
+	// //////////////////////////////////////
+	
+	private String nombreSector;
+
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	@RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*")
+	@DescribedAs("Nombre del Sector:")
+	@MemberOrder(sequence = "10")
+	public String getNombreSector() {
+		return nombreSector;
+	}
+
+	public void setNombreSector(final String nombreSector) {
+		this.nombreSector = nombreSector;
+	}
+
+	// //////////////////////////////////////
+	// creadoPor
+	// //////////////////////////////////////
+
+	private String creadoPor;
+
+	@Hidden(where = Where.ALL_TABLES)
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(final String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+
+	// //////////////////////////////////////
+	// Habilitado
+	// //////////////////////////////////////
+
+	public char habilitado;
+
+	@Hidden
+	@MemberOrder(name = "Detalles", sequence = "9")
+	public char getEstaHabilitado() {
+		return habilitado;
+	}
+
+	public void setHabilitado(final char habilitado) {
+		this.habilitado = habilitado;
+	}
+	
+	// //////////////////////////////////////
+	// Eliminar
+	// //////////////////////////////////////
+	/**
+	 * Método que utilizo para deshabilitar un Insumo.
+	 * 
+	 * @return la propiedad habilitado en false.
+	 */
+	@Named("Eliminar")
+	@PublishedAction
+	@Bulk
+	@MemberOrder(name = "accionEliminar", sequence = "1")
+	public List<Sector> eliminar() {
+			//setHabilitado(false);
+			container.flush();
+			container.warnUser("Eliminado " + container.titleOf(this));
+		return sectorRepositorio.listAll();
+	}
+	
+	// //////////////////////////////////////
+	// Comparable
+	// //////////////////////////////////////
+	/**
+	 * Implementacion de la interface comparable, necesaria para toda entidad.
+	 *  
+	 */
+	@Override
+	public int compareTo(final Sector sector) {
+		return ObjectContracts.compare(this, sector, "nombreSector");
+	}
+	
+	// //////////////////////////////////////
+	// Injected Services
+	// //////////////////////////////////////
+
+	@Inject
+	private DomainObjectContainer container;
+	
+	@Inject
+	private SectorRepositorio sectorRepositorio;
+}
---
+++ dom/src/main/java/dom/sector/SectorRepositorio.java
@@ -0,0 +1,163 @@
+/*
+ * This is a software made for inventory control
+ * 
+ * Copyright (C) 2014, ProyectoTypes
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * 
+ * 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package dom.sector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.ActionSemantics;
+import org.apache.isis.applib.annotation.ActionSemantics.Of;
+import org.apache.isis.applib.annotation.Bookmarkable;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.MinLength;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.annotation.Prototype;
+import org.apache.isis.applib.annotation.RegEx;
+import org.apache.isis.applib.query.QueryDefault;
+
+@DomainService(menuOrder = "100")
+@Named("SECTOR")
+public class SectorRepositorio {
+
+	public SectorRepositorio() {
+
+	}
+
+	// //////////////////////////////////////
+	// Identification in the UI
+	// //////////////////////////////////////
+
+	public String getId() {
+		return "sector";
+	}
+
+	public String iconName() {
+		return "Sector";
+	}
+
+	// //////////////////////////////////////
+	// Insertar
+	// //////////////////////////////////////
+
+	@Named("Agregar")
+	@MemberOrder(sequence = "10")
+	public Sector create(
+			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Nombre") String nombreSector) {
+		return nuevoSector(nombreSector, this.currentUserName());
+	}
+
+	@Programmatic
+	public Sector nuevoSector(final String nombreSector, final String creadoPor) {
+		final Sector unSector = this.container.newTransientInstance(Sector.class);
+		unSector.setNombreSector(nombreSector.toUpperCase().trim());
+		unSector.setHabilitado('S');
+		unSector.setCreadoPor(creadoPor);
+		this.container.persistIfNotAlready(unSector);
+		this.container.flush();
+		return unSector;
+
+	}
+
+	// //////////////////////////////////////
+	// ListarSectores
+	// //////////////////////////////////////   
+	
+    @Bookmarkable
+    @ActionSemantics(Of.SAFE)
+    @MemberOrder(sequence = "20")
+    @Named ("Listar Sector")
+    public List<Sector> listAll() {
+        return filtroAL(container.allMatches(new QueryDefault<Sector>(Sector.class,
+				"ListarSectores")),'S');
+    }
+    
+	private List<Sector> filtroAL(List<Sector> Sectores, char S)
+	{
+		List<Sector> filtroAL=new ArrayList<Sector>();
+		
+		for(Sector sec:Sectores)
+		{
+			if(sec.getEstaHabilitado()==S)
+				filtroAL.add(sec);
+		}
+		
+		return filtroAL;
+	}
+
+	/**
+	 * Buscar
+	 * @param nombreSector
+	 * @return
+	 */
+	@MemberOrder(sequence = "21")
+	public List<Sector> buscar(
+			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Nombre") @MinLength(2) String nombreSector) {
+		final List<Sector> listarSectores = this.container
+				.allMatches(new QueryDefault<Sector>(Sector.class,
+						"buscarPorNombre", "nombreSector", nombreSector
+								.toUpperCase().trim()));
+		if (listarSectores.isEmpty())
+			this.container
+					.warnUser("No se encontraron sectores cargados en el sistema.");
+		return listarSectores;
+	}
+
+	// //////////////////////////////////////
+	// AutoComplete
+	// //////////////////////////////////////
+	
+	@Programmatic
+	public List<Sector> autoComplete(final String buscarNombreSector) {
+		return container.allMatches(new QueryDefault<Sector>(Sector.class,
+				"autoCompletePorNombreSector", "nombreSector",
+				buscarNombreSector.toUpperCase().trim()));
+	}
+
+	@Prototype
+	public List<Sector> crearSectores() {
+		List<Sector> sectores = new ArrayList<Sector>();
+		sectores.add(this.create("Administración"));
+		sectores.add(this.create("Informatica"));
+		sectores.add(this.create("Ventas"));
+		sectores.add(this.create("Recepción"));
+		sectores.add(this.create("RRHH"));
+		return sectores;
+	}
+
+	// //////////////////////////////////////
+	// CurrentUserName
+	// //////////////////////////////////////
+
+	private String currentUserName() {
+		return container.getUser().getName();
+	}
+
+	// //////////////////////////////////////
+	// Injected Services
+	// //////////////////////////////////////
+
+	@javax.inject.Inject
+	private DomainObjectContainer container;
+}
