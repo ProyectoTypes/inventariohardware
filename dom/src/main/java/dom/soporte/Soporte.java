c0536b7c25046e980e96c08a9390b59a0ea7cb01
Exequiel Wiedermann <EWFWiedermann@gmail.com>
2014-12-21 19:57:04 -0300

    Ordenando las relaciones en Soporte.

--- dom/src/main/java/dom/soporte/Soporte.java
+++ dom/src/main/java/dom/soporte/Soporte.java
@@ -119,7 +119,7 @@ public class Soporte implements Comparable<Soporte> {
 	// ////////////////////////////////////////////////////
 
 	public String title() {
-		return "SOPORTE "+this.getComputadora().getPlacaDeRed().getIp();
+		return "SOPORTE "+this.getComputadora().getHardware().getGabinete().getPlacaDeRed().getIp();
 	}
 
 	public String iconName() {
--- dom/src/main/java/dom/soporte/SoporteRepositorio.java
+++ dom/src/main/java/dom/soporte/SoporteRepositorio.java
@@ -115,8 +115,7 @@ public class SoporteRepositorio {
 			final String observaciones) {
 
 		List<Soporte> soporte = container.allMatches(new QueryDefault<Soporte>(
-				Soporte.class, "seEncuentraEnReparacion", "ip", computadora.getPlacaDeRed()
-						.getIp()));
+				Soporte.class, "seEncuentraEnReparacion", "ip", computadora.getHardware().getGabinete().getPlacaDeRed().getIp()));
 		if (soporte.isEmpty())
 			return null;
 		return "La computadora se encuentra en reparacion.";
@@ -180,7 +179,7 @@ public class SoporteRepositorio {
 	public List<Soporte> buscarPorIp(
 			@Named("Computadora") Computadora computadora) {
 		return container.allMatches(new QueryDefault<Soporte>(Soporte.class,
-				"buscarPorIp", "ip", computadora.getPlacaDeRed().getIp()));
+				"buscarPorIp", "ip", computadora.getHardware().getGabinete().getPlacaDeRed().getIp()));
 	}
 
 	public List<Computadora> choices0BuscarPorIp() {
